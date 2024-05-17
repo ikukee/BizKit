c4db30690d77e38fd6ab28d81088884830e6956f
ikukee <rmpanis2@gmail.com>
2024-05-17 18:23:22 +0800

    modified the logical error in match.py for a faster performance
    Changed Match.py  on is_match function
    "to optimize the matching algorithm so that users can find out if they have a match faster."
    Stated in readme

--- phasebook/match.py
+++ phasebook/match.py
@@ -23,5 +23,5 @@ def is_match(fave_numbers_1, fave_numbers_2):
     for number in fave_numbers_2:
         if number not in fave_numbers_1:
             return False
-
-    return True
+        else:
+            return True
